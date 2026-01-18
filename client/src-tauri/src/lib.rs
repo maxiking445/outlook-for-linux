ef52787e013b9dd10406d15099496bc72cec0a94
Max <Maximilianlakke@googlemail.com>
2026-01-18 14:40:58 +0100

    feat: add tray menue

--- client/src-tauri/src/lib.rs
+++ client/src-tauri/src/lib.rs
@@ -1,17 +1,46 @@
-#[cfg_attr(mobile, tauri::mobile_entry_point)]
+use tauri::menu::MenuItemBuilder;
+use tauri::{
+    menu::{MenuBuilder},
+    tray::TrayIconBuilder,
+    Manager, WindowEvent,
+};
 
 pub fn run() {
-  tauri::Builder::default()
-    .setup(|app| {
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Info)
-            .build(),
-        )?;
-      }
-      Ok(())
-    })
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .on_window_event(|window, event| {
+            if let WindowEvent::CloseRequested { api, .. } = event {
+                api.prevent_close();
+                let _ = window.hide();
+            }
+        })
+        .setup(|app| {
+            let hide = MenuItemBuilder::new("Hide").id("hide").build(app).unwrap();
+            let show = MenuItemBuilder::new("Show").id("show").build(app).unwrap();
+            let quit = MenuItemBuilder::new("Quit").id("quit").build(app).unwrap();
+            let menu = MenuBuilder::new(app)
+                .items(&[&hide, &show, &quit])
+                .build()
+                .unwrap();
+
+            TrayIconBuilder::new()
+                .icon(app.default_window_icon().unwrap().clone())
+                .menu(&menu)
+                .on_menu_event(|app, event| match event.id().as_ref() {
+                    "quit" => app.exit(0),
+                    "hide" => {
+                        let window = app.get_webview_window("main").unwrap();
+                        window.hide().unwrap();
+                    }
+                    "show" => {
+                        let window = app.get_webview_window("main").unwrap();
+                        window.show().unwrap();
+                        let _ = window.set_focus();
+                    }
+                    _ => {}
+                })
+                .build(app)?;
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("Something wrong when running tauri application");
 }
